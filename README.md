3c6ddab2ce9e0c252fa66cd79ebde5a836426f30
Jon Wedell <jonwedell@gmail.com>
2013-11-25 13:04:02 -0600

    Fully working. Still no parallelism.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ vbrfs
 =====
 
 Transcodes FLAC files to MP3 V0 files as they are read.
+
+Does a few tricks to make the process seem more quick than it really is.
--- vbrfs.py
+++ vbrfs.py
@@ -1,16 +1,33 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+
+# Copyright (c) 2013 Jonathan Wedell <jonwedell@gmail.com> (author)
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 import os
 import sys
+
 import errno
 import StringIO
 import optparse
 import subprocess
-
+import multiprocessing
 from fuse import FUSE, FuseOSError, Operations
 
 
-class v0Convert(Operations):
+
+
+class vbrConvert(Operations):
 
     def __init__(self, root):
         self.root = root
@@ -26,17 +43,24 @@ class v0Convert(Operations):
         return path
 
     def _absolutePath(self, partial):
-        return os.path.abspath(self._full_path(partial)).replace('.mp3','.flac').replace('.MP3','.FLAC')
+
+        if os.path.isfile(self._full_path(partial)):
+            return os.path.abspath(self._full_path(partial))
+        else:
+            return os.path.abspath(self._full_path(partial)).replace('.mp3','.flac').replace('.MP3','.FLAC')
 
     def convFile(self, path):
         # We don't need to convert a given file twice
         if path in self.known_files:
             return
 
+        if os.path.isfile(self._full_path(path)):
+            return
+
         # Get the actual path
         abspath = self._absolutePath(path)
 
-        # Don't do anything for non-mp3 files
+        # Don't do anything for non-flac files
         if not ".flac" in abspath and not ".FLAC" in abspath:
             return
 
@@ -51,7 +75,7 @@ class v0Convert(Operations):
         # Then get the decoded FLAC stream
         flac = subprocess.Popen(['flac', '-c', '-d', abspath],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         # Then set up the lame arguments and call lame
-        lamecmd = ['lame','-q0', '-V0', '--vbr-new', '--ignore-tag-errors', '--add-id3v2', '--pad-id3v2', '--ignore-tag-errors', '--ta', tags.get('artist','?'), '--tt', tags.get('title','?'), '--tl', tags.get('album','?'), '--tg', tags.get('genre','?'), '--tn', tags.get('tracknumber','?'), '--ty', tags.get('date','?'), '-', '-']
+        lamecmd = ['lame','-q0', '-V', options.v, '--vbr-new', '--ignore-tag-errors', '--add-id3v2', '--pad-id3v2', '--ignore-tag-errors', '--ta', tags.get('artist','?'), '--tt', tags.get('title','?'), '--tl', tags.get('album','?'), '--tg', tags.get('genre','?'), '--tn', tags.get('tracknumber','?'), '--ty', tags.get('date','?'), '-', '-']
         lame = subprocess.Popen(lamecmd,stdout=subprocess.PIPE,stdin=flac.stdout,stderr=subprocess.PIPE)
         # Create a file-like object with the resulting mp3 stream
         self.known_files[path] = StringIO.StringIO(lame.stdout.read())
@@ -71,16 +95,19 @@ class v0Convert(Operations):
 
     def chmod(self, path, mode):
         if options.debug: print "chmod(",path,",",mode,")"
-        full_path = self._full_path(path)
-        return os.chmod(full_path, mode)
+        raise OSError(38,"Not implemented.")
 
     def chown(self, path, uid, gid):
         if options.debug: print "chown(",path,",",uid,",",gid,")"
-        full_path = self._full_path(path)
-        return os.chown(full_path, uid, gid)
+        raise OSError(38,"Not implemented.")
 
     def getattr(self, path, fh=None):
         if options.debug: print "getattr(",path,",",fh,")"
+
+        # Make sure they can't grab the actual FLACs
+        if path[-5:] == ".flac" or path[-5:] == ".FLAC":
+            raise OSError(2,"No such file or directory.")
+
         full_path = self._absolutePath(path)
 
         # Only convert files for getattr requests if option enabled
@@ -121,16 +148,15 @@ class v0Convert(Operations):
 
     def mknod(self, path, mode, dev):
         if options.debug: print "mknod(",path,",",mode,",",dev,")"
-        return os.mknod(self._full_path(path), mode, dev)
+        raise OSError(38,"Not implemented.")
 
     def rmdir(self, path):
         if options.debug: print "rmdir(",path,")"
-        full_path = self._full_path(path)
-        return os.rmdir(full_path)
+        raise OSError(38,"Not implemented.")
 
     def mkdir(self, path, mode):
         if options.debug: print "mkdir(",path,",",mode,")"
-        return os.mkdir(self._full_path(path), mode)
+        raise OSError(38,"Not implemented.")
 
     def statfs(self, path):
         if options.debug: print "statfs(",path,")"
@@ -142,23 +168,23 @@ class v0Convert(Operations):
 
     def unlink(self, path):
         if options.debug: print "unlink(",path,")"
-        return os.unlink(self._full_path(path))
+        raise OSError(38,"Not implemented.")
 
     def symlink(self, target, name):
         if options.debug: print "symlink(",target,",",name,")"
-        return os.symlink(self._full_path(target), self._full_path(name))
+        raise OSError(38,"Not implemented.")
 
     def rename(self, old, new):
         if options.debug: print "rename(",old,",",new,")"
-        return os.rename(self._full_path(old), self._full_path(new))
+        raise OSError(38,"Not implemented.")
 
     def link(self, target, name):
         if options.debug: print "link(",taget,",",name,")"
-        return os.link(self._full_path(target), self._full_path(name))
+        raise OSError(38,"Not implemented.")
 
     def utimens(self, path, times=None):
         if options.debug: print "utimens(",path,",",times,")"
-        return os.utime(self._full_path(path), times)
+        raise OSError(38,"Not implemented.")
 
     # File methods
     # ============
@@ -166,13 +192,15 @@ class v0Convert(Operations):
     def open(self, path, flags):
         if options.debug: print "open(",path,",",flags,")"
 
+        if flags == 34817:
+            raise OSError(38,"Not implemented.")
+
         full_path = self._absolutePath(path)
         return os.open(full_path, flags)
 
     def create(self, path, mode, fi=None):
         if options.debug: print "create(",path,",",mode,",",fi,")"
-        full_path = self._full_path(path)
-        return os.open(full_path, os.O_WRONLY | os.O_CREAT, mode)
+        raise OSError(38,"Not implemented.")
 
     def read(self, path, length, offset, fh):
         if options.debug: print "read(",path,",",length,",",offset,",",fh,")"
@@ -189,19 +217,16 @@ class v0Convert(Operations):
             return os.read(fh, length)
 
     def write(self, path, buf, offset, fh):
-        if options.debug: print "write(",path,",",buf,",",offset,",",fh,")"
-        os.lseek(fh, offset, os.SEEK_SET)
-        return os.write(fh, buf)
+        if options.debug: print "write(",path,",[BUFF],",offset,",",fh,")"
+        raise OSError(38,"Not implemented.")
 
     def truncate(self, path, length, fh=None):
         if options.debug: print "truncate(",path,",",length,",",fh,")"
-        full_path = self._full_path(path)
-        with open(full_path, 'r+') as f:
-            f.truncate(length)
+        raise OSError(38,"Not implemented.")
 
     def flush(self, path, fh):
         if options.debug: print "flush(",path,",",fh,")"
-        return os.fsync(fh)
+        raise OSError(38,"Not implemented.")
 
     def release(self, path, fh):
         if options.debug: print "release(",path,",",fh,")"
@@ -209,26 +234,37 @@ class v0Convert(Operations):
 
     def fsync(self, path, fdatasync, fh):
         if options.debug: print "fsync(",path,",",fdatasync,",",fh,")"
-        return self.flush(path, fh)
+        raise OSError(38,"Not implemented.")
 
 
 if __name__ == '__main__':
 
     # Specify some basic information about our command
-    usage = "usage: vbrfs [options] flacdir mp3dir"
+    usage = "usage: %prog [options] flacdir mp3dir"
     parser = optparse.OptionParser(usage=usage,version="%prog .1",description="This program will present all FLACS as VBR mp3s. Like mp3fs but with VBR.")
 
+    # Set up the option groups
+    basic = optparse.OptionGroup(parser,"Basic options","If the default isn't good enough for you.")
+    parser.add_option_group(basic)
+    advanced = optparse.OptionGroup(parser,"Advanced options","You may want to use some of these.")
+    parser.add_option_group(advanced)
     devel = optparse.OptionGroup(parser,"Developer options","You really shouldn't use these unless you know what you are doing.")
     parser.add_option_group(devel)
 
     # Specify the common arguments
-    parser.add_option("--attrbconv", action="store_true", dest="attrbconv", default=False, help="Will convert flac->mp3 even for things like ls. Not recomended.")
-    parser.add_option("--foreground", action="store_true", dest="foreground", default=False, help="Run this command in the foreground.")
+    basic.add_option("--foreground", action="store_true", dest="foreground", default=False, help="Run this command in the foreground.")
+    basic.add_option("--v", action="store", dest="v", type="choice", default="0", choices=(map(lambda x:str(x),range(0,10))), help="What V level do you want to transcode to? Default: %default.")
+    advanced.add_option("--threads", action="store", dest="threads", default=multiprocessing.cpu_count()-1, type="int", help="How many threads should we use? This ideally should be set to one fewer than the number of cores you have available. Default: %default")
+    devel.add_option("--always-conv", action="store_true", dest="attrbconv", default=False, help="Will convert flac->mp3 even for things like 'ls'. Only needed if you want 'ls' to show the right file size, but doing so will be very slow. (You are forcing vbrfs to transcode a directory just to 'ls'.)")
     devel.add_option("--debug", action="store_true", dest="debug", default=False, help="Print every action sent to the filesystem to stdout.")
 
     # Options, parse 'em
     (options, args) = parser.parse_args()
 
+    if options.debug:
+        options.foreground = True
+
+    # Check for invalid command line options
     if len(args) < 2:
         print "You must specify the flac directory and the mp3 directory."
         sys.exit(0)
@@ -237,4 +273,4 @@ if __name__ == '__main__':
         sys.exit(0)
 
     # Make the magic happen
-    FUSE(v0Convert(args[0]), args[1], foreground=options.foreground)
+    FUSE(vbrConvert(args[0]), args[1], foreground=options.foreground)
