2bfef7454c456aa17ef781cb06fdb38c04ebc6cf
Jon Wedell <jonwedell@gmail.com>
2014-04-20 16:02:16 -0500

    Small updates.

--- vbrfs.py
+++ vbrfs.py
@@ -363,7 +363,8 @@ class vbrConvert(Operations):
     def statfs(self, path):
         logger.debug("statfs( " + unicode(path) + " )")
         full_path = self._full_path(path)
-        stv = os.statvfs(full_path)
+	stv = os.statvfs(full_path)
+
         return dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
             'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
             'f_frsize', 'f_namemax'))
